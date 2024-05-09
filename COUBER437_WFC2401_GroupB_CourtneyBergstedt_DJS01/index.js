3f9e5841483c153d68d1de17b4869495eb5d3762
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-05-09 12:31:48 +0200

    Added comments and changed naming conventions

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS01/index.js
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS01/index.js
@@ -1,17 +1,11 @@
-/**
- * Debugging Guide
- * 1. Make the code more readable
- * 2. Pick up calculation errors
- * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const initialVelocity = 10000; // Initial velocity (km/h)
+const acceleration = 3; // Acceleration (m/s^2)
+const time = 3600; // Time (seconds) (1 hour)
+const initialDistance = 0; // Initial distance (km)
+const initialFuel = 5000; //Initial fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
 const d2 = d + (vel*time) //calcultes new distance
